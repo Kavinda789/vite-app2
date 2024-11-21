b259131134b4457519cd8e1df249f058ff4af46e
Kavinda789 <127042694+Kavinda789@users.noreply.github.com>
2024-11-21 22:47:44 +0530

    11/21-first commite

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div>
+      <div>
+        {/* hero section */}
+
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/assets/assets.js
@@ -0,0 +1,6 @@
+export const Menu = [
+  { id: 1, name: "Home", link: "#" },
+  { id: 1, name: "Shop", link: "#" },
+  { id: 1, name: "About", link: "#" },
+  { id: 1, name: "Blogs", link: "#" },
+];
