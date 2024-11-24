11ccc156cdf8f816f41608faefe138c99e7a54e5
Kavinda789 <127042694+Kavinda789@users.noreply.github.com>
2024-11-24 23:52:36 +0530

    11/24-create herobar section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,76 @@
 import React from "react";
+import Slider from "react-slick";
+import { SliderData } from "../../assets/assets";
+import Button from "../Shape/Button";
 
 function Hero() {
-  return (
-    <div>
-      <div>
-        {/* hero section */}
+  const settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    cssEase: "ease-in-out",
+    pauseOnHover: false,
+    pauseOnFocus: true,
+  };
 
+  return (
+    <div className="container">
+      <div
+        className="overflow-hidden rounded-3xl min-h-[550px]
+      sm:min-h-[650px] hero-bg-color flex justify-center items-center"
+      >
+        <div className="container pb-8 sm:pb-0">
+          {/* hero section */}
+          <Slider {...settings}>
+            {SliderData.map((data) => (
+              <div key={data.id}>
+                <div className="grid grid-cols-1 sm:grid-cols-2">
+                  {/* text Content Section */}
+                  <div
+                    className="flex flex-col justify-center gap-4 sm:pt-3
+                  text-center sm:text-left order-2 sm:order-1 relative z-10"
+                  >
+                    <h1 className="text-2xl sm:text-6xl lg:text-2xl font-bold">
+                      {data.subtitle}
+                    </h1>
+                    <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
+                      {data.title}
+                    </h1>
+                    <h1
+                      className="text-5xl uppercase text-white sm:text-[80px]
+                    md:text-[100px] xl:text-[150px] font-bold"
+                    >
+                      {data.title2}
+                    </h1>
+                    <div className="pl-3">
+                      <Button
+                        text="Shop By Catergory"
+                        bgColor="bg-primary"
+                        textColor="text-white"
+                      />
+                    </div>
+                  </div>
+                  {/* img Section */}
+                  <div className="order-1 sm:order-2">
+                    <div>
+                      <img
+                        src={data.img}
+                        alt=""
+                        className="w-[300px] h-[300px] sm:h-[450px] lg:scale-110
+                    object-contain mx-auto drop-shadow-[-8px_4px_6px_rgb(0,0,0,.4)]
+                    relative z-40"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
       </div>
     </div>
   );
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,3 +1,34 @@
+import Slider_1 from "./img_slider_1.png";
+import Slider_2 from "./img_slider_2.png";
+import Slider_3 from "./img_slider_3.png";
+
+export const SliderData = [
+  {
+    id: 1,
+    img: Slider_1,
+    subtitle: "Beats Solo",
+    title: "Wireless",
+    title2: "Headphone",
+    description: "",
+  },
+  {
+    id: 2,
+    img: Slider_2,
+    subtitle: "Beats Solo",
+    title: "Branded",
+    title2: "Laptop",
+    description: "",
+  },
+  {
+    id: 3,
+    img: Slider_3,
+    subtitle: "Beats Solo",
+    title: "Wireless",
+    title2: "Virtual",
+    description: "",
+  },
+];
+
 export const Menu = [
   { id: 1, name: "Home", link: "#" },
   { id: 1, name: "Shop", link: "#" },
